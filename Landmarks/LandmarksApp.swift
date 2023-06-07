e371caf14baa0589b8bb79fec55c60504b33e322
git-shochann <sho_825.sa216@docomo.ne.jp>
2023-06-07 17:09:48 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sho Tsuboya on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
