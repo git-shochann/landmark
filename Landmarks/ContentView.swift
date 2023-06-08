f7ca5fb783c522f471d7497bfcadc8937db43408
git-shochann <sho_825.sa216@docomo.ne.jp>
2023-06-08 23:46:26 +0900

    struct, protocol, variable

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -16,13 +16,16 @@ import SwiftUI
 // 新しくContentView構造体を作成する
 // Viewプロトコルを満たしている
 struct ContentView: View {
-    // bodyが要求される some Viewは型
-    var body: some View
-}
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    // body変数が要求される その型はsome View
+    //
+    var body: some View {
+        Image("myHDRimage").allowedDynamicRange(.high)
     }
 }
+
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
